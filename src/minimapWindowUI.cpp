38bcd48d9291ae7ad341c13a63b13a8ff84b019d
schlumpf <schlumpf@modcraft>
2010-09-04 22:50:47 +0200

    Fixed loading files from disc on unix, where paths are seperated by / and not \.

---
+++ src/minimapWindowUI.cpp
@@ -0,0 +1,10 @@
+#include "noggit.h"
+
+#include "minimapWindowUI.h"
+
+#include "textUI.h"
+
+
+minimapWindowUI::minimapWindowUI( float px, float py, float w, float h ) : window( px, py, w, h )
+{
+}
