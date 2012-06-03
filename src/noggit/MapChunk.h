b6610cb860c9badbf190a172d30fcaf0b89af698
hanfer <hanfer@localhost>
2012-06-03 03:12:46 +0200

    advance time with normal speed

---
+++ src/noggit/MapChunk.h
@@ -0,0 +1,159 @@
+// MapChunk.h is part of Noggit3, licensed via GNU General Publiicense (version 3).
+// Bernd Lörwald <bloerwald+noggit@googlemail.com>
+// Glararan <glararan@glararan.eu>
+// Stephan Biegel <project.modcraft@googlemail.com>
+// Tigurius <bstigurius@googlemail.com>
+
+#ifndef MAPCHUNK_H
+#define MAPCHUNK_H
+
+#include <math/vector_4d.h>
+
+#include <noggit/MapTile.h> // MapTile
+
+namespace noggit
+{
+  class blp_texture;
+  namespace mpq
+  {
+    class file;
+  }
+}
+class brush;
+class Frustum;
+class World;
+class Skies;
+
+typedef unsigned short StripType;
+
+static const int mapbufsize = 9*9 + 8*8;
+
+class MapChunk
+{
+
+public:
+  MapTile *mt;
+  ::math::vector_3d vmin, vmax, vcenter;
+  int px, py;
+
+  MapChunkHeader header;
+  bool Changed;
+  size_t nTextures;
+
+  float xbase, ybase, zbase;
+  float r;
+
+  bool mBigAlpha;
+
+  int nameID;
+
+  unsigned int Flags;
+
+  unsigned int areaID;
+
+  bool haswater;
+
+  int holes;
+
+  int tex[4];
+  noggit::blp_texture* _textures[4];
+  unsigned int texFlags[4];
+  unsigned int effectID[4];
+  unsigned int MCALoffset[4];
+  unsigned char amap[3][64*64];
+  unsigned char mShadowMap[8*64];
+  GLuint alphamaps[3];
+  GLuint shadow;
+
+  int animated[4];
+
+  GLuint vertices, normals, minimap, minishadows;
+
+  StripType *strip;
+  int striplen;
+
+  MapChunk(World* world, MapTile* mt, noggit::mpq::file* f,bool bigAlpha);
+  ~MapChunk();
+
+  void destroy();
+  void initStrip();
+
+  bool is_visible ( const float& cull_distance
+                  , const Frustum& frustum
+                  , const ::math::vector_3d& camera
+                  ) const;
+
+  void draw (bool draw_terrain_height_contour
+            , bool mark_impassable_chunks
+            , bool draw_area_id_overlay
+            , bool dont_draw_cursor
+            , const Skies* skies);
+  void drawContour();
+  void drawAreaID();
+  void drawBlock();
+  void drawSelect();
+  void drawNoDetail();
+  void drawPass(int anim);
+  // todo split into draw_lines and draw_hole_lines
+  void drawLines (bool draw_hole_lines);
+
+  void drawTextures();
+
+  void recalcNorms();
+
+  ::math::vector_3d mNormals[mapbufsize];
+  ::math::vector_3d mVertices[mapbufsize];
+  //! \todo Is this needed? Can't we just use the real vertices?
+  ::math::vector_3d mMinimap[mapbufsize];
+  ::math::vector_4d mFakeShadows[mapbufsize];
+
+  void getSelectionCoord(float *x,float *z);
+  float getSelectionHeight();
+
+  ::math::vector_3d GetSelectionPosition();
+
+  bool changeTerrain(float x, float z, float change, float radius, int BrushType);
+  bool flattenTerrain(float x, float z, float h, float remain, float radius, int BrushType);
+  bool blurTerrain(float x, float z, float remain, float radius, int BrushType);
+
+  bool paintTexture(float x, float z, const brush& Brush, float strength, float pressure, noggit::blp_texture* texture);
+  int addTexture(noggit::blp_texture* texture);
+  void switchTexture( noggit::blp_texture* oldTexture, noggit::blp_texture* newTexture );
+  void eraseTextures();
+
+  bool isHole(int i,int j);
+  void addHole(int i,int j);
+  void removeHole(int i,int j);
+
+  void setFlag(bool on_or_off, int flag);
+
+  int getAreaID();
+  void setAreaID(int ID);
+
+  bool GetVertex(float x,float z, ::math::vector_3d* V);
+
+  void loadTextures();
+//  char getAlpha(float x,float y);
+
+
+  //float getTerrainHeight(float x, float z);
+
+  void SetAnim (int anim) const;
+  void RemoveAnim (int anim) const;
+
+  void GenerateContourMap();
+  void CreateStrips();
+
+private:
+  World* _world;
+
+  GLuint _contour_texture;
+  float _contour_coord_gen[4];
+
+  StripType _odd_strips[8*18];
+  StripType _even_strips[8*18];
+  StripType _line_strip[32];
+  StripType _hole_strip[128];
+};
+
+#endif // MAPCHUNK_H
---
+++ src/noggit/MapTile.h
@@ -0,0 +1,114 @@
+// MapTile.h is part of Noggit3, licensed via GNU General Publiicense (version 3).
+// Bernd Lörwald <bloerwald+noggit@googlemail.com>
+// Mjollnà <mjollna.wow@gmail.com>
+// Stephan Biegel <project.modcraft@googlemail.com>
+// Tigurius <bstigurius@googlemail.com>
+
+#ifndef MAPTILE_H
+#define MAPTILE_H
+
+#include <vector>
+#include <map>
+#include <string>
+
+#include <opengl/types.h>
+
+#include <noggit/MapHeaders.h>
+#include <noggit/World.h> // instances types
+
+class Frustum;
+class Liquid;
+class World;
+class MapChunk;
+
+class QRectF;
+
+namespace math
+{
+  class vector_3d;
+}
+
+class Skies;
+
+class MapTile
+{
+public:
+  MapTile( World*, int x0, int z0, const std::string& pFilename, bool pBigAlpha );
+  ~MapTile();
+
+  //! \brief Get the maximum height of terrain on this map tile.
+  float getMaxHeight();
+
+  //! \brief Get chunk for sub offset x,z.
+  MapChunk* getChunk( unsigned int x, unsigned int z );
+
+  int modelCount;
+  int mPositionX;
+  int mPositionZ;
+  float xbase, zbase;
+
+  bool changed;
+
+  void draw ( bool draw_terrain_height_contour
+            , bool mark_impassable_chunks
+            , bool draw_area_id_overlay
+            , bool dont_draw_cursor
+            , const Skies* skies
+            , const float& cull_distance
+            , const Frustum& frustum
+            , const ::math::vector_3d& camera
+            );
+  void drawSelect ( const float& cull_distance
+                  , const Frustum& frustum
+                  , const ::math::vector_3d& camera
+                  );
+  void drawLines ( bool draw_hole_lines
+                 , const float& cull_distance
+                 , const Frustum& frustum
+                 , const ::math::vector_3d& camera
+                 );
+  void drawWater (const Skies* skies);
+  void drawTextures (const QRectF& chunks_to_draw) const;
+  void drawMFBO();
+
+  bool GetVertex( float x, float z, ::math::vector_3d *V );
+
+  void saveTile ( const World::model_instances_type::const_iterator& models_begin
+                , const World::model_instances_type::const_iterator& models_end
+                , const World::wmo_instances_type::const_iterator& wmos_begin
+                , const World::wmo_instances_type::const_iterator& wmos_end
+                );
+  void saveTileCata ( const World::model_instances_type::const_iterator& models_begin
+                    , const World::model_instances_type::const_iterator& models_end
+                    , const World::wmo_instances_type::const_iterator& wmos_begin
+                    , const World::wmo_instances_type::const_iterator& wmos_end
+                    );
+
+  bool isTile( int pX, int pZ );
+  void clearAllModels();
+
+private:
+  // MFBO:
+  GLfloat mMinimumValues[3*3*3];
+  GLfloat mMaximumValues[3*3*3];
+
+  // MHDR:
+  int mFlags;
+  bool mBigAlpha;
+
+  // Data to be loaded and later unloaded.
+  std::vector<std::string> mTextureFilenames;
+  std::vector<std::string> mModelFilenames;
+  std::vector<std::string> mWMOFilenames;
+
+  std::string mFilename;
+
+  MapChunk * mChunks[16][16];
+  std::vector<Liquid*> mLiquids;
+
+  World* _world;
+
+  friend class MapChunk;
+};
+
+#endif
